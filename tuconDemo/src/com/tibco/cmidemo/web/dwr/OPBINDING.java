f8ff11c3c1ab34deac1285e83a1131c67b1115a0
rioliu <rioliu@b323eb75-3582-63a9-068f-d84617b43f09>
2010-05-04 12:55:55 +0000

    - add function to get available operation list

--- tuconDemo/src/com/tibco/cmidemo/web/dwr/OPBINDING.java
+++ tuconDemo/src/com/tibco/cmidemo/web/dwr/OPBINDING.java
@@ -1,6 +1,13 @@
 package com.tibco.cmidemo.web.dwr;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
+
+import org.apache.commons.lang.StringUtils;
 
 import com.tibco.cmidemo.dao.ColumnConst;
 import com.tibco.cmidemo.hibernate.GiOpbinding;
@@ -15,6 +22,8 @@ import com.tibco.cmidemo.web.WebAppException;
  */
 public class OPBINDING extends DWR {
 
+    private static Properties installed_operations = null;
+    
     public static List<GiOpbinding> getOpBindingList(String parentId) throws WebAppException {
         
         return getList(GiOpbinding.class, parentId, ColumnConst.PBV_BININDEX);
@@ -23,6 +32,40 @@ public class OPBINDING extends DWR {
     public static GiOpbinding getOpBindingByID(long id) throws WebAppException {
         
        return getById(GiOpbinding.class, id);
+        
+    }
+    
+    public static List<String> getAvailableOpList(String prot, String parentId) throws WebAppException {
+        
+        if(installed_operations == null) {
+            
+            InputStream inStream = OPBINDING.class.getClassLoader().getResourceAsStream("/com/tibco/cmidemo/web/dwr/installed_operations.properties");
+            installed_operations = new Properties();
+            try {
+                installed_operations.load(new InputStreamReader(inStream));
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        
+        List<String> availabled = new ArrayList<String>();
+        List<String> selected = new ArrayList<String>();
+        List<GiOpbinding> temp = getOpBindingList(parentId);
+        for(GiOpbinding opb : temp) {
+            selected.add(opb.getTxId());
+        }
+        
+        String opIds = (String) installed_operations.get(prot);
+        if(StringUtils.isBlank(opIds))
+            throw new WebAppException("can not find operations of protocol [" + prot + "]");
+        
+        for(String name : opIds.split(",")) {
+            if(!selected.contains(name))
+                availabled.add(name);
+        }
+        
+        return availabled;
         
     }
     
