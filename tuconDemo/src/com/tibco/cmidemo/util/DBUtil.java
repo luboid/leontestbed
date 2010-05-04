81391cb0be6dae1a15344adbac68880f35912ab7
rioliu <rioliu@b323eb75-3582-63a9-068f-d84617b43f09>
2010-05-04 07:57:53 +0000

    - add db util

---
+++ tuconDemo/src/com/tibco/cmidemo/util/DBUtil.java
@@ -0,0 +1,29 @@
+package com.tibco.cmidemo.util;
+
+import com.tibco.cmidemo.dao.ColumnConst;
+import com.tibco.cmidemo.dao.Condition;
+import com.tibco.cmidemo.dao.Criteria;
+import com.tibco.cmidemo.hibernate.GiPartner;
+
+/**
+ * @author xliu May 4, 2010
+ *
+ */
+public class DBUtil {
+
+    public static <T> Criteria createCriteria(Class<T> clazz, String parentColumn, String parentId) {
+        
+        Criteria c = new Criteria(clazz);
+        c.addCondition(new Condition(parentColumn, Condition.EQUALS, parentId));
+        
+        return c;
+    }
+    
+    public static <T> Criteria createCriteria(String paCat) {
+        
+        Criteria c = new Criteria(GiPartner.class);
+        c.addCondition(new Condition(ColumnConst.CATEGORY, Condition.EQUALS, paCat));
+        
+        return c;
+    }
+}
