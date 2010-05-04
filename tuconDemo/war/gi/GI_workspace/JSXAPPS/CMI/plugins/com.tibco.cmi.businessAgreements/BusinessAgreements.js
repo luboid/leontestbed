ec69256c34dfcc57ebeff49a7bf2e7f2cc91fef8
zouljs <zouljs@b323eb75-3582-63a9-068f-d84617b43f09>
2010-05-04 14:58:50 +0000

    wuweijun

--- tuconDemo/war/gi/GI_workspace/JSXAPPS/CMI/plugins/com.tibco.cmi.businessAgreements/BusinessAgreements.js
+++ tuconDemo/war/gi/GI_workspace/JSXAPPS/CMI/plugins/com.tibco.cmi.businessAgreements/BusinessAgreements.js
@@ -2,12 +2,53 @@
     jsx3.gui.Block,
     null,
     function(BusinessAgreements,instance){
-      // var systemUtil = com.tibco.cmi.system.util;
-      // var server = systemUtil.getServer();
+			
+			var systemUtil = com.tibco.cmi.system.util;
+      var server = systemUtil.getServer();
+			var dwrEngine = com.tibco.cmi.dwr.Engine;
+			var dwrService = com.tibco.cmi.dwr.Service;
 
-			instance.onRsrcLoad = function(){
+			instance.onRsrcLoad = function(){				
 				this.getServer().subscribe("showAgreementsList", this, this.showAgreementsList);
+				this.getAgreementList();
 			};
+		
+			instance.getAgreementList = function(){
+				var me = this;
+				var service = dwrEngine.loadService('BIZAGREEMENT','getBAList',[]);
+				service.subscribe(dwrService.ON_SUCCESS, me, me._callback_getBAList_onSuccess);
+				service.doCall();
+			}
+			
+			instance._callback_getBAList_onSuccess = function(objEvent) {
+				var agreementList = objEvent.data;
+				var agreementcdf = new jsx3.xml.Document();
+				var root = agreementcdf.createDocumentElement("data");
+				root.setAttribute("jsxid", "jsxroot");
+				
+				for(var i = 0;i < agreementList.length;i++) {	
+					var record = root.createNode(jsx3.xml.Entity.TYPEELEMENT, "record");
+					var item = agreementList[i];
+					var agreementId = item.binindex;
+					var displayName = item.displayName;
+					var isValid = item.isValid;
+					var status;
+					if(isValid)
+						status = "Active";
+					else
+						status = "Inactive";
+					
+					record.setAttribute("jsxid", jsx3.xml.CDF.getKey());
+					record.setAttribute("checked",0);
+					record.setAttribute("agreementId", agreementId);
+					record.setAttribute("name", displayName);
+					record.setAttribute("status", status);
+					
+					root.appendChild(record);
+				}
+				server.getJSXByName("mtxAgreements").setSourceXML(agreementcdf);
+			  server.getJSXByName("mtxAgreements").repaint();
+			}
 		
       instance.newAgreements = function() {
         var server = com.tibco.cmi;
@@ -40,8 +81,65 @@
       }
 			
 			instance.showAgreementsList = function() {
-				this.getDescendantOfName("blkAddEdit").removeChildren();
+				this.getAgreementList();
+				server.getJSXByName("blkAddEdit").removeChildren();
 				var masterDetail = com.tibco.cmi.getJSXByName("mainLayout");
         masterDetail.setSubcontainer1Pct("100%",true);
+			}
+			
+			instance.deleteBA = function() {
+				var itemList = this.getSelectedItem();
+				var BAs = [];
+				for(var i = 0; i < itemList.size(); i++) {
+					var item = itemList.get(i);
+					BAs.push(item.getAttribute("agreementId"));
+				}
+				
+				var service = dwrEngine.loadService('BIZAGREEMENT','removeBA',[BAs]);
+				service.subscribe(dwrService.ON_SUCCESS, function(){server.getJSXByName("blkBusinessAgreements").getAgreementList()});
+				service.doCall();
+			}
+			
+			instance.setEnabled = function(btnEnable, btnRemove) { 
+				var mtx = com.tibco.cmi.getJSXByName("mtxAgreements");
+				var idList = mtx.getSortedIds();
+				var enabled = 0;
+				for(var j=0;j<idList.length;j++){
+					var recordID = idList[j];
+					var item = mtx.getRecordNode(recordID);
+					if(item.getAttribute("checked") == 0) {
+						enabled = 1;				
+						break;
+					}					
+				}
+				
+				for(var i = 0; i < idList.length;i++) {
+					var recordID = idList[i];
+					var item = mtx.getRecordNode(recordID);
+					item.setAttribute("checked",enabled);
+					mtx.redrawRecord(recordID, jsx3.xml.CDF.UPDATE);	
+				}		
+					
+				btnRemove.setEnabled(enabled);
+				if(enabled) { 					
+					btnEnable.setText("Disable All");	
+				}
+				else {
+					btnEnable.setText("Enable All");
+				}					
+			}
+			
+			instance.changeState = function(btn) {
+				if(this.getSelectedItem().size() == 0)
+					btn.setEnabled(0);
+				else
+				  btn.setEnabled(1);
+			}
+			
+			instance.getSelectedItem = function() {
+				var mtx = com.tibco.cmi.getJSXByName("mtxAgreements");
+				var xml = mtx.getXML();
+				var itemList = xml.selectNodes("//record[@checked='1']");
+				return itemList;
 			}
     })
--- tuconDemo/war/gi/GI_workspace/JSXAPPS/CMI/plugins/com.tibco.cmi.newAgreement/NewAgreement.js
+++ tuconDemo/war/gi/GI_workspace/JSXAPPS/CMI/plugins/com.tibco.cmi.newAgreement/NewAgreement.js
@@ -2,11 +2,173 @@ jsx3.lang.Class.defineClass("com.tibco.cmi.newAgreement.NewAgreement",
     jsx3.gui.Block,
     null,
     function(NewAgreement,instance){
+			var systemUtil = com.tibco.cmi.system.util;
+      var server = systemUtil.getServer();
+			var dwrEngine = com.tibco.cmi.dwr.Engine;
+			var dwrService = com.tibco.cmi.dwr.Service;
+			
+			instance.onRsrcLoad = function(){
+				NewAgreement.block = server.getJSXByName("blkNewAgreement");
+				this.getPartnerList();
+			};
+			
+			instance.getPartnerList = function() {
+				var me = this;
+				var hostService = dwrEngine.loadService('PARTNER','getHostPAList',[]);
+				hostService.subscribe(dwrService.ON_SUCCESS, me, me._callback_getHostPAList_onSuccess);
+				hostService.doCall();
+				
+				var partnerService = dwrEngine.loadService('PARTNER','getPartnerPAList',[]);
+				partnerService.subscribe(dwrService.ON_SUCCESS, me, me._callback_getPartnerPAList_onSuccess);
+				partnerService.doCall();
+			}
+			
+			instance._callback_getHostPAList_onSuccess = function(objEvent) {
+				var partnerList = objEvent.data;
+				
+				var hostCDF = new jsx3.xml.Document();
+				var hostRoot = hostCDF.createDocumentElement("data");
+				hostRoot.setAttribute("jsxid", "jsxroot");
+				
+				if(partnerList){
+					for(var i = 0;i < partnerList.length;i++) {					
+						var item = partnerList[i];
+						var type = item.category;
+						var name = item.name;
+						var id = item.binindex;
+						
+						var record = hostRoot.createNode(jsx3.xml.Entity.TYPEELEMENT, "record");
+						record.setAttribute("jsxid", jsx3.xml.CDF.getKey());
+						record.setAttribute("id", id);
+						record.setAttribute("radioed", 0);
+						record.setAttribute("name", name);
+						record.setAttribute("type", type);
+						hostRoot.appendChild(record);
+					}
+					server.getJSXByName("mtxHost").setSourceXML(hostCDF);
+					server.getJSXByName("mtxHost").repaint();
+					
+					for(var j = 0;j < partnerList.length;j++) {					
+						var item = partnerList[j];
+						var type = item.category;
+						var partnerId = item.binindex;
+						
+						var service = dwrEngine.loadService('PROTOCOL','getEnabledProtocolList',[partnerId]);
+						service.subscribe(dwrService.ON_SUCCESS, function(objEvent){
+																										   var protocolList = objEvent.data;
+																											 if(protocolList)
+																											   NewAgreement.block.setProtocol(protocolList, hostCDF, server.getJSXByName("mtxHost"));
+																										 });
+						service.doCall();
+					}
+				}				
+			}
+			
+			instance._callback_getPartnerPAList_onSuccess = function(objEvent) {
+				var partnerList = objEvent.data;
+				
+				var partnerCDF = new jsx3.xml.Document();
+				var partnerRoot = partnerCDF.createDocumentElement("data");
+				partnerRoot.setAttribute("jsxid", "jsxroot");
+				
+				if(partnerList){
+					for(var i = 0;i < partnerList.length;i++) {					
+						var item = partnerList[i];
+						var type = item.category;
+						var name = item.name;
+						var id = item.binindex;
+						
+						var record = partnerRoot.createNode(jsx3.xml.Entity.TYPEELEMENT, "record");
+						record.setAttribute("jsxid", jsx3.xml.CDF.getKey());
+						record.setAttribute("id", id);
+						record.setAttribute("radioed", 0);
+						record.setAttribute("name", name);
+						record.setAttribute("type", type);
+						partnerRoot.appendChild(record);
+					}
+					server.getJSXByName("mtxPartner").setSourceXML(partnerCDF);
+					server.getJSXByName("mtxPartner").repaint();
+					
+					for(var j = 0;j < partnerList.length;j++) {					
+						var item = partnerList[j];
+						var type = item.category;
+						var partnerId = item.binindex;
+						
+						var service = dwrEngine.loadService('PROTOCOL','getEnabledProtocolList',[partnerId]);
+						service.subscribe(dwrService.ON_SUCCESS, function(objEvent){
+																										   var protocolList = objEvent.data;
+																											 if(protocolList)
+																											   NewAgreement.block.setProtocol(protocolList, partnerCDF, server.getJSXByName("mtxPartner"));
+																										 });
+						service.doCall();
+					}
+				}				
+			}
+			
+			instance.setProtocol = function(protocolList, cdf, mtx) {
+			  var partnerId;
+				var displayProtocol;
+				for(var i = 0; i < protocolList.length; i++) {
+					var protocol = protocolList[i];
+					partnerId = procotol.tpBinindex;
+					var name = protocol.name;
+					displayProtocol += name;
+					
+					if(i != protocolList.length - 1)
+						displayProtocol += ",";
+				}
+				
+				var itemList = cdf.selectNodes("//record[@id='" + partnerId + "']");
+				for(var j = 0; j < itemList.size(); j++) {
+					var item = itemList.get(j);
+					item.setAttribute("protocol", displayProtocol);
+					var recordID = item.getAttribute("jsxid");
+					mtx.redrawRecord(recordID, jsx3.xml.CDF.UPDATE);	
+				}
+			}
+			
 			instance.saveAgreement = function(){
-				this.getServer().publish({subject:"showAgreementsList"});				
+				var mtxHost = com.tibco.cmi.getJSXByName("mtxHost");
+				var xml = mtxHost.getXML();
+				var hostParty = xml.selectNodes("//record[@radioed='1']");
+				var hostPartyId;
+				if(hostParty.size() > 0)
+					hostPartyId = hostParty.get(0).getAttribute("id");
+				
+				var mtxPartner = com.tibco.cmi.getJSXByName("mtxPartner");
+				var xml = mtxPartner.getXML();
+				var partnerParty = xml.selectNodes("//record[@radioed='1']");
+				var partnerPartyId;
+				if(partnerParty.size() > 0)
+					partnerPartyId = partnerParty.get(0).getAttribute("id");
+				
+				if(hostParty.size() > 0 && partnerParty.size() > 0) {
+					var BA = new Object;
+					BA.HBinindex = hostPartyId;
+					BA.tpBinindex = partnerPartyId;
+					
+					var service = dwrEngine.loadService('BIZAGREEMENT','saveBA',[BA]);
+					service.subscribe(dwrService.ON_SUCCESS, function(){server.publish({subject:"showAgreementsList"});	});
+					service.doCall();
+					
+					
+				} else {
+					alert("Host or Partner party is not selected")
+				}
+							
 			}
 			
 			instance.cancel = function() {
 				this.getServer().publish({subject:"showAgreementsList"});	
 			}
+			
+			instance.getProtocolList = jsx3.$Y(function(cb){
+				var partnerId = cb.args()[0];
+				var service = dwrEngine.loadService('PROT','getProtocolList',[partnerId]);
+				service.subscribe(dwrService.ON_SUCCESS, function(objEvent){
+																									var protocol = objEvent.data;
+																									cb.done(protocol);
+																								});
+				service.doCall();
+			})
     })
