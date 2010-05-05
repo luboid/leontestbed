3b2683b0529fb6458968cd56832c7203ed4eb2d3
rioliu <rioliu@b323eb75-3582-63a9-068f-d84617b43f09>
2010-05-05 08:04:25 +0000

    - missed sql file

---
+++ tuconDemo/ddl/gi_operations.sql
@@ -0,0 +1,4 @@
+﻿insert into gi_operations(operation_name, protocol_name) values("BC/1.0/Notify", "EZComm")
+insert into gi_operations(operation_name, protocol_name) values("Manage Widgets/1.0/buyWidget", "EZComm")
+insert into gi_operations(operation_name, protocol_name) values("Manage Widgets/1.0/getWidgetQuote", "EZComm")
+insert into gi_operations(operation_name, protocol_name) values("Widget Inventory/1.0/haveWidget", "EZComm")
