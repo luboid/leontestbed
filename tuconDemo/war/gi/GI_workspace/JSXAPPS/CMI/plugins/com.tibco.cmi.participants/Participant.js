97ed835eb504416ce659f1939dac83e08269b593
zouljs <zouljs@b323eb75-3582-63a9-068f-d84617b43f09>
2010-05-06 12:42:59 +0000

    ok

--- tuconDemo/war/gi/GI_workspace/JSXAPPS/CMI/plugins/com.tibco.cmi.participants/Participant.js
+++ tuconDemo/war/gi/GI_workspace/JSXAPPS/CMI/plugins/com.tibco.cmi.participants/Participant.js
@@ -5,11 +5,12 @@ jsx3.lang.Class.defineClass('com.tibco.cmi.participants.Participant',
 	 
 	 var dwrEngine = com.tibco.cmi.dwr.Engine;
 	 var dwrService = com.tibco.cmi.dwr.Service;
+	 var intervalId ='';
 	
 	  var systemUtil = com.tibco.cmi.system.util;
 	  instance.onRsrcLoad = function(){
 		   setTimeout(jsx3.$F(function(){this.init();
-			 this.getParticipants();
+			 setInterval(function(){PARTICIPANT.me.getParticipants();},5000);
 			 }).bind(this),500);
 			 
 		};
@@ -25,12 +26,11 @@ jsx3.lang.Class.defineClass('com.tibco.cmi.participants.Participant',
 			 PARTICIPANT.protocal = this.getDescendantOfName('blkprotocal');
 		   PARTICIPANT.ADD = 1;
 			 PARTICIPANT.EDIT = 0;
+			 PARTICIPANT.DOMAINID_EDIT_OR_ADD = '';
 			 PARTICIPANT.LOCATION_EDIT_OR_ADD = '';
 			 PARTICIPANT.CONTACT_EDIT_OR_ADD = '';
 			 PARTICIPANT.EDIT_OR_ADD = '';
 			 PARTICIPANT.currentPaId = '';
-			 
-			 
 		};
 		
 		instance.getParticipants = function(){
@@ -188,7 +188,6 @@ jsx3.lang.Class.defineClass('com.tibco.cmi.participants.Participant',
 		};
 		
 		instance._callback_saveLocation_onSuccess = function(objEvent){
-		   debugger;
 		   this.getLocationList();
 		}
 		
@@ -383,14 +382,8 @@ jsx3.lang.Class.defineClass('com.tibco.cmi.participants.Participant',
 			 form.appendChild(binindex);
 			 form.appendChild(alias);
 			 form.submit();
-			 dlg.doClose();
-			 	this.getCertList();
-
+			 intervalId = setInterval(function(){PARTICIPANT.me.minitorUpload()},100);
 		};
-		
-
-    		
-
 		
 		instance.getProtocal = function(){
 		   var service = dwrEngine.loadService('PROTOCOL','getEnabledProtocolList',[PARTICIPANT.currentPaId]);
@@ -400,6 +393,7 @@ jsx3.lang.Class.defineClass('com.tibco.cmi.participants.Participant',
 		
 		instance._callback_getProtocal_onSuccess = function(objEvent){
 		   var list = objEvent.data;
+			 PARTICIPANT.protocolList = list;
 			 var mtx = PARTICIPANT.protocal.getDescendantOfName('mtxprotocal');
        var cdf = jsx3.xml.CDF.Document.newDocument(); 			 
 			 if(list&&list.length>0){
@@ -517,7 +511,7 @@ jsx3.lang.Class.defineClass('com.tibco.cmi.participants.Participant',
 		};
 		
 		instance.getIdentityList = function(){
-			var service = dwrEngine.loadService('DOMAINID','getDomainIdList',[PARTICIPANT.currentPaId]);
+			var service = dwrEngine.loadService('DOMAINID','getDomainIdListByParentID',[PARTICIPANT.currentPaId]);
 			service.subscribe(dwrService.ON_SUCCESS,PARTICIPANT.me,PARTICIPANT.me._callback_getDomainIdList_onSuccess);
 			service.doCall();
 		}
@@ -545,7 +539,276 @@ jsx3.lang.Class.defineClass('com.tibco.cmi.participants.Participant',
 			}
 			this.getDescendantOfName('mtxidentifierlist').setSourceXML(cdf);
 			this.getDescendantOfName('mtxidentifierlist').repaintData();
+		};
+		
+		instance.loadIdentityDlg = function(){
+		   var container = this.getServer().getBodyBlock();
+		   var dlg = this.getPlugIn().loadRsrcComponent('IdentityDlg_xml',container);
+			 
+			 var service = dwrEngine.loadService('DOMAIN','getDomainList',[]);
+				service.subscribe(dwrService.ON_SUCCESS,PARTICIPANT.me,PARTICIPANT.me._callback_getDomainList_onSuccess);
+				service.doCall();
+			
+			 return dlg;
+		};
+		
+		instance._callback_getDomainList_onSuccess = function(objEvent){
+			var list = objEvent.data;
+			
+			var cdf = new jsx3.xml.Document();
+			var root = cdf.createDocumentElement("data");
+			root.setAttribute("jsxid", "jsxroot");
+			for(var i = 0; i < list.length; i++) {
+				var domain = list[i];
+				var record = root.createNode(jsx3.xml.Entity.TYPEELEMENT, "record");
+				
+				record.setAttribute("jsxid", domain);
+				record.setAttribute("jsxtext", domain);					
+				root.appendChild(record);
+			}
+			
+			systemUtil.getServer().getJSXByName("selDomain").setSourceXML(cdf);
+			systemUtil.getServer().getJSXByName("selDomain").repaint();
+		};
+		
+		instance.saveDomainId = function(dlg){
+			var domain = systemUtil.getServer().getJSXByName("selDomain").getValue();
+			var identity = systemUtil.getServer().getJSXByName("txtIdentity").getValue();
+			var domainId = new Object;
+			domainId.domid = identity;
+			domainId.CDomid = identity;
+			domainId.domaintype = domain;
+			domainId.PBinindex = PARTICIPANT.currentPaId;
+			//domainId.PBinindex = parentId;
+			
+			var service = dwrEngine.loadService('DOMAINID','saveDomainId',[domainId]);
+			service.subscribe(dwrService.ON_SUCCESS, function(){PARTICIPANT.me.getIdentityList()});
+			service.doCall();	
+			dlg.doClose();
+		};
+		
+		instance.minitorUpload = function(){
+				iframe_upload
+				var iframe;
+				var IFrameInnerHTML = '';
+				iframe = frames["iframe_upload"];
+			  var content = iframe.status;
+				if(content == 'OK'){
+				   systemUtil.getServer().getJSXByName("dlgupload").doClose();
+					 if(intervalId){
+					     clearInterval(intervalId);
+					 }
+					 this.getCertList();
+				}
+
+       
 		}
+		
+		
+		instance.removeDomainId = function(){
+			var mtx = PARTICIPANT.mtxidentifierlist;
+			var xml = mtx.getXML();
+			var itemList = xml.selectNodes("//record[@checked='1']");
+			
+			var dis = [];
+				for(var i = 0; i < itemList.size(); i++) {
+					var item = itemList.get(i);
+					dis.push(item.getAttribute("domainId"));
+				}
+				
+				var service = dwrEngine.loadService('DOMAINID','removeDomainId',[dis]);
+				service.subscribe(dwrService.ON_SUCCESS, function(){PARTICIPANT.me.getIdentityList()});
+				service.doCall();
+		};
+		
+		instance.editProtocol = function(){
+			systemUtil.getServer().getJSXByName("editProtocolLayout"). setSubcontainer1Pct("0%",true);
+			var mtx = systemUtil.getServer().getJSXByName("mtxprotocal");
+			
+			var protocolId = mtx.getSelectedNodes().get(0).getAttribute("binindex");
+			for(var i = 0; i < PARTICIPANT.protocolList.length;i++) {
+				if(PARTICIPANT.protocolList[i].binindex == protocolId){
+					PARTICIPANT.selectedProtocol = PARTICIPANT.protocolList[i];
+					break;
+				}
+			}
+			
+			var service ;
+			if(PARTICIPANT.selectedProtocol.name == "EZComm") {
+				systemUtil.getServer().getJSXByName("lalIdentityTitle").setText("AS2 Identifier",true)
+				service = dwrEngine.loadService('DOMAINID','getDomainIdListByAS2Flag',[PARTICIPANT.currentPaId,true]);
+			}
+			else {
+				systemUtil.getServer().getJSXByName("lalIdentityTitle").setText("Identifier",true)
+			  service = dwrEngine.loadService('DOMAINID','getDomainIdListByParentID',[PARTICIPANT.currentPaId]);
+			}
+			service.subscribe(dwrService.ON_SUCCESS,PARTICIPANT.me,PARTICIPANT.me._callback_getDomainIdList_onSuccess_editProtocol);
+			service.doCall();
+
+			if(PARTICIPANT.selectedProtocol.emailId) {
+				PARTICIPANT.hasEmail = true;
+				var emailService = dwrEngine.loadService('EMAILMONIKER','getEmailByID',[PARTICIPANT.selectedProtocol.emailId]);
+				emailService.subscribe(dwrService.ON_SUCCESS,PARTICIPANT.me,PARTICIPANT.me._callback_getEmailByID_onSuccess);
+				emailService.doCall();
+			}
+			else {
+				PARTICIPANT.hasEmail = false;
+				systemUtil.getServer().getJSXByName("txtEmailList").setValue("",true);
+			}
+		}
+		
+		instance._callback_getDomainIdList_onSuccess_editProtocol = function(objEvent){
+			var list = objEvent.data;
+			var cdf = new jsx3.xml.Document();
+			var root = cdf.createDocumentElement("data");
+			root.setAttribute("jsxid", "jsxroot");
+			
+			for(var i = 0; i < list.length; i++) {
+				var domain = list[i];
+				var record = root.createNode(jsx3.xml.Entity.TYPEELEMENT, "record");
+				
+				var domainId = domain.binindex;
+				var display = domain.domaintype + "-" + domain.CDomid;
+				
+				record.setAttribute("jsxid", domainId);
+				record.setAttribute("jsxtext", display);					
+				root.appendChild(record);
+			}
+			
+			systemUtil.getServer().getJSXByName("selIdentifier").setSourceXML(cdf);
+			
+			if(PARTICIPANT.selectedProtocol.name == "EZComm")
+				systemUtil.getServer().getJSXByName("selIdentifier").setValue(PARTICIPANT.selectedProtocol.as2Binindex);
+			else
+			  systemUtil.getServer().getJSXByName("selIdentifier").setValue(PARTICIPANT.selectedProtocol.DBinindex);
+			
+		}
+		
+		instance._callback_getEmailByID_onSuccess = function(objEvent) {
+			var emailList = objEvent.data.email;
+			PARTICIPANT.email = objEvent.data;
+			systemUtil.getServer().getJSXByName("txtEmailList").setValue(emailList,true);
+		}
+		
+		instance.saveProtocol = function(){
+			var emailList = systemUtil.getServer().getJSXByName("txtEmailList").getValue();
+			var email;
+			if(PARTICIPANT.hasEmail) {
+				email = PARTICIPANT.email;
+				email.email = emailList;
+			}
+			else {
+				email = new Object;
+				email.EBinindex = PARTICIPANT.selectedProtocol.binindex;
+				email.tpBinindex = PARTICIPANT.selectedProtocol.tpBinindex;
+				email.email = emailList;
+			}
+			
+			var emailService = dwrEngine.loadService('EMAILMONIKER','saveEmail',[email]);
+			emailService.subscribe(dwrService.ON_SUCCESS,PARTICIPANT.me,PARTICIPANT.me._callback_saveEmail_onSuccess);
+			emailService.doCall();
+		}
+		
+		instance._callback_saveEmail_onSuccess = function(objEvent) {
+			var email = objEvent.data;
+			var emailId = email.binindex;
+			var domainIDBinindex = systemUtil.getServer().getJSXByName("selIdentifier").getValue();
+			
+			PARTICIPANT.selectedProtocol.emailId = emailId;
+			
+			if(domainIDBinindex) {
+				if(PARTICIPANT.selectedProtocol.name == "EZComm")
+					PARTICIPANT.selectedProtocol.as2Binindex = domainIDBinindex;
+				else
+					PARTICIPANT.selectedProtocol.DBinindex = domainIDBinindex;
+			}
+			
+			var protocolList = [PARTICIPANT.selectedProtocol];
+			protocolList.push()
+			var service = dwrEngine.loadService('PROTOCOL','enableProtocols',[protocolList]);
+			service.subscribe(dwrService.ON_SUCCESS, function(){PARTICIPANT.me.showProtocolList()});
+			service.doCall();
+		}
+
+		instance.showProtocolList = function(){
+			systemUtil.getServer().getJSXByName("editProtocolLayout"). setSubcontainer1Pct("100%",true);
+		};
+		
+		instance.getTransport = function(){
+		   var protocalId = PARTICIPANT.selectedProtocol.binindex;
+		   var service = dwrEngine .loadService('TRANSPORT','getTransportList',[protocalId]);
+			 service.subscribe(dwrService.ON_SUCCESS,function(objEvent){PARTICIPANT.me._callback_getTransport_onSuccess(objEvent);})
+			 service.doCall();
+		};
+		
+		instance._callback_getTransport_onSuccess = function(objEvent){
+		   var list = objEvent.data;
+			 var mtx = systemUtil.getServer().getJSXByName("mtxtransport");
+			 var cdf = jsx3.xml.CDF.Document.newDocument();
+			 if(list&&list.length>0){
+			     for(var i= 0;i<list.length;i++){
+					    list[i].jsxid = jsx3.xml.CDF.getKey();
+							cdf.insertRecord(list[i]);
+					 } 
+					 mtx.setSourceXML(cdf);
+			 }
+			 mtx.repaintData();
+		}
+		
+		instance.addTransport = function(){
+		   var container = this.getServer().getBodyBlock();
+			 var protId = PARTICIPANT.selectedProtocol.binindex;
+			 var dlg = this.getPlugIn().loadRsrcComponent('TransportEdit_xml',container);
+			 var cdfcontainer = dlg.getDescendantOfName('cdftransport');
+			 var cdf = jsx3.xml.CDF.Document.newDocument();
+			 cdf.insertRecord({jsxid:'transport',type:'FILE',status:1,fileprocessing:'File Mask',EBinindex:protId});
+			 cdfcontainer.setSourceXML(cdf);
+			 cdfcontainer.read();  
+			 return dlg;
+		};
+		
+		instance.editTransport = function(){
+		   var dlg = this.addTransport();
+			 var cdfcontainer = dlg.getDescendantOfName('cdftransport');
+			 var mtx = systemUtil.getServer().getJSXByName("mtxtransport");
+			 var record = mtx.getRecord(mtx.getSelectedNodes().get(0).getAttribute('jsxid'));
+			 record['jsxid'] = 'transport';
+			 cdfcontainer.getXML().insertRecord(record,'jsxroot');
+			 cdfcontainer.read();
+			 var type = dlg.getDescendantOfName('seltype').getValue();
+			 this.selectTranportType(type);
+		};
+		
+		instance.selectTranportType = function(id){
+		    var dlg = systemUtil.getServer().getJSXByName("dlgtransport");
+				if(id == 'HTTP'){
+				   dlg.getDescendantOfName('httpPane').setDisplay(jsx3.gui.Block.DISPLAYBLOCK,true);
+				}
+				else {
+				    dlg.getDescendantOfName('httpPane').setDisplay(jsx3.gui.Block.DISPLAYNONE,true);
+				}
+				
+		}
+		
+		instance.saveTransport = function(){
+		   debugger;
+		   var dlg = systemUtil.getServer().getJSXByName("dlgtransport");
+			 var cdfcontainer = dlg.getDescendantOfName('cdftransport');
+			 cdfcontainer.write();
+			 var record = cdfcontainer.getRecord('transport');
+			 delete record['jsxid'];
+			 delete record['jsxselected'];
+			 delete record['lastmodified'];
+			 var service = dwrEngine.loadService('TRANSPORT','saveTransport',[record]);
+			 service.subscribe(dwrService.ON_SUCCESS,function(objEvent){PARTICIPANT.me._callback_saveTransport_onSuccess(objEvent)});
+			 service.doCall();
+			 dlg.doClose();
+		};
+		instance._callback_saveTransport_onSuccess = function(objEvent){
+		   this.getTransport();
+		}
+		
+
 		
 		
 	}
