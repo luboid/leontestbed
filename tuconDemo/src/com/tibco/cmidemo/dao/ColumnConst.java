185b0499eaeae33715c020636c21e9a2b22d709c
rioliu <rioliu@b323eb75-3582-63a9-068f-d84617b43f09>
2010-05-06 01:50:14 +0000

    - update HBinindex constant

--- tuconDemo/src/com/tibco/cmidemo/dao/ColumnConst.java
+++ tuconDemo/src/com/tibco/cmidemo/dao/ColumnConst.java
@@ -14,6 +14,7 @@ public interface ColumnConst {
     public static final String L_BININDEX = "LBinindex";
     public static final String P_BININDEX = "PBinindex";
     public static final String CATEGORY = "category";
+    public static final String HBinindex = "HBinindex";
     
     public static final String TRANS_TYPE_HTTP = "HTTP";
     public static final String TRANS_TYPE_FILE = "FILE";
